1e7496275ae45e144e0a0f807abfd29f47c2625b
Denis Lykov <cailean393@gmail.com>
2018-05-20 19:17:22 +0300

    add socket config for users

--- server/socket/global.js
+++ server/socket/global.js
@@ -1,6 +1,6 @@
-module.exports = function(io){
+let users = {};
 
-    let users = {};
+module.exports = function(io){
 
     io.on('connection', function(socket){
 
@@ -11,17 +11,67 @@ module.exports = function(io){
             // socket.leave(room);
         });
 
-        socket.on('room', function(room) {
-            socket.join(room);
-        });
-
-        socket.on('room_message', (data) => {
-            let res = JSON.parse(data);
-            socket.broadcast.to(res.chat_id).emit('room_message', data);
-        });
+        chatSocket(socket);
+        friendSocket(socket, users);
 
     });
 
+}
+
+function chatSocket(socket) {
+
+    socket.on('room', function(room) {
+        socket.join(room);
+    });
+
+    socket.on('room_message', (data) => {
+        let res = JSON.parse(data);
+        socket.broadcast.to(res.chat_id).emit('room_message', data);
+    });
+
+}
+
+function friendSocket(socket) {
+
+    socket.on('invite', (data) => {
+    
+        let d = JSON.parse(data);
+
+        if (users[d.id]) users[d.id].emit('invited', JSON.stringify(d.user));
+
+    });
+
+    socket.on('cancel_invite', (data) => {
+
+        let d = JSON.parse(data);
+
+        if (users[d.id]) users[d.id].emit('invite_canceled', JSON.stringify(d.user));
+
+    })
+
+    socket.on('reject_invite', (data) => {
+
+        let d = JSON.parse(data);
+
+        if (users[d.id]) users[d.id].emit('invite_rejected', JSON.stringify(d.user));
+
+    })
+
+    socket.on('add_friend', (data) => {
+
+        let d = JSON.parse(data);
+
+        if (users[d.id]) users[d.id].emit('friend_added', JSON.stringify(d.user));
+
+    })
+
+    socket.on('del_friend', (data) => {
+
+        let d = JSON.parse(data);
+
+        if (users[d.id]) users[d.id].emit('friend_deleted', JSON.stringify(d.user));
+
+    })
 
 }
     
